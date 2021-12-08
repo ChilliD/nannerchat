5a8e5dddd32b3cca739da2f534e89da8c2d87bae
ChilliD <87444703+ChilliD@users.noreply.github.com>
2021-12-08 02:30:23 -0500

    user validation and password hashing

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const User = require('../schemas/UserSchema');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -12,13 +14,35 @@ router.get('/', (req, res, next) => {
     res.status(200).render('register');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
 
-    let username = req.body.logUsername.trim();
-    let password = req.body.logPassword;
+    let username = req.body.username.trim();
+    let password = req.body.password;
     let payload = req.body;
 
     if (username && password) {
+        let user = await User.findOne({ username: username })
+        .catch((error) => {
+            console.log(error);
+            payload.errorMsg = "Something went wrong";
+            res.status(200).render('register', payload);
+        });
+
+        if (user == null) {
+    
+            let data = req.body;
+
+            data.password = await bcrypt.hash(password, 10);
+
+            User.create(data)
+            .then((user) => {
+                console.log(user);
+            })
+
+        } else {
+            payload.errorMsg = "Username already exists";
+            res.status(200).render('register', payload);
+        }
 
     } else {
         payload.errorMsg = "Please fill in each field";
---
+++ schemas/UserSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePic: {
+        type: String,
+        default: '/images/profilePic.png'
+    }
+}, { timestamps: true });
+
+let User = mongoose.model('User', UserSchema);
+module.exports = User;
